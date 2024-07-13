b39fa68810109068563bf19bb43e66ba79c118f7
Laksh Shetty <lakshshetty16@gmail.com>
2024-07-13 14:19:25 +0530

    Security and design improvements.

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
 import logging
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Configuration for Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'lakshshetty16@gmail.com'
-app.config['MAIL_PASSWORD'] = 'yqkc crcg hgsc jmfj'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 mail = Mail(app)
 
